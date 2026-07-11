9dd5905b9ad4592ccc68c14d4022e376eac46153
Henry4F <lehoullierprutzmanra3616@gmail.com>
2026-07-11 14:42:31 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  },
+  transports: ['websocket', 'polling'],
+  pingTimeout: 60000,
+  pingInterval: 25000
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
